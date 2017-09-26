886e6626f0c3f5dce33b262b74a1157d3da68074
Andy Tong <Hurricane@Awesome-Sht.local>
2017-09-26 02:17:32 -0700

    ok

--- UIImageExtension.podspec
+++ UIImageExtension.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'UIImageExtension'
-  s.version          = '0.1.0'
+  s.version          = '0.1.2'
   s.summary          = 'A short description of UIImageExtension.'
 
 # This description is used to generate tags and improve search results.
--- UIImageExtension/Classes/UIImage+Extension.swift
+++ UIImageExtension/Classes/UIImage+Extension.swift
@@ -16,3 +16,22 @@ public extension UIImage {
         self.init(named: name, in: resourceBundle, compatibleWith: nil)
     }
 }
+
+extension Bundle {
+    
+    /// The resource bundle lives within a pod framework
+    static func resourceBundle(_ user: AnyObject) -> Bundle? {
+        let podBundle = Bundle(for: type(of: user))
+        
+        // resource bundle it's within podBundle, its name is yourFramework.bundle
+        let path = podBundle.bundlePath
+        let bundePath = (path as NSString).lastPathComponent
+        let bundleName = bundePath.components(separatedBy: ".").first
+        
+        guard let url = podBundle.url(forResource: bundleName, withExtension: "bundle") else {
+            //            fatalError("Can not find bundle:\(type(of: user)).bundle")
+            return Bundle.main
+        }
+        return Bundle(url: url)
+    }
+}
